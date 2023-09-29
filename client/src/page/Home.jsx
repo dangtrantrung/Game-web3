c9241953aee13dcd73581c36d0d2136198d5c7ed
dangtrantrung <trung280584@gmail.com>
2023-09-29 11:00:34 +0700

    Player Register

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -37,11 +37,25 @@ const Home = () => {
     }
   };
   return (
-    <div className="flex flex-col">
-      {/* <h1 className="text-5xl p-3">Avax Gods</h1>
+    /*  <div className="flex flex-col">
+      <h1 className="text-5xl p-3">Avax Gods</h1>
       <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
-      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p> */}
+      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
       <h1 className="text-xl text-white">{demo}</h1>
+    </div> */
+    <div className="flex flex-col">
+      <CustomInput
+        label="Name"
+        placeHolder="Enter your player name"
+        value={playerName}
+        handleValueChange={setPlayerName}
+      />
+
+      <CustomButton
+        title="Register"
+        handleClick={handleClick}
+        restStyles="mt-6"
+      />
     </div>
   );
 };
