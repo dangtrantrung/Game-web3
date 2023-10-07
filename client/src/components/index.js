746c7a475004771e1a56b75d5c819a506a71892a
dangtrantrung <trung280584@gmail.com>
2023-10-07 11:40:13 +0700

    PlayerInfo

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,5 +3,19 @@ import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
 import Alert from "./Alert";
 import GameLoad from "./GameLoad";
+import Card from "./Card";
+import GameInfo from "./GameInfo";
+import ActionButton from "./ActionButton";
+import PlayerInfo from "./PlayerInfo";
 
-export { PageHOC, CustomInput, CustomButton, Alert, GameLoad };
+export {
+  PageHOC,
+  CustomInput,
+  CustomButton,
+  Alert,
+  GameLoad,
+  GameInfo,
+  ActionButton,
+  Card,
+  PlayerInfo,
+};
--- client/src/page/Battle.jsx
+++ client/src/page/Battle.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { CustomButton, CustomInput, PageHOC, GameLoad } from "../components";
+import {
+  CustomButton,
+  CustomInput,
+  PageHOC,
+  GameLoad,
+  Card,
+  ActionButton,
+  PlayerInfo,
+  GameInfo,
+} from "../components";
 import { useNavigate, useParams } from "react-router-dom";
 import styles from "../styles";
 import { useGlobalContext } from "../context";
@@ -13,7 +22,7 @@ import {
   player02 as player02Icon,
 } from "../assets";
 import { playAudio } from "../utils/animation";
-import { player01, player02 } from "../assets";
+import { player01 as player01img, player02 as player02img } from "../assets";
 
 const Battle = () => {
   const {
@@ -25,8 +34,8 @@ const Battle = () => {
     battleGround,
     setBattleGround,
   } = useGlobalContext();
-  const [player1, setPlayer1] = useState("");
-  const [player2, setPlayer2] = useState("");
+  const [player1, setPlayer1] = useState({});
+  const [player2, setPlayer2] = useState({});
   const { battleName } = useParams();
 
   const navigate = useNavigate();
@@ -59,7 +68,38 @@ const Battle = () => {
           console.log("player1", player1);
           console.log("player2", player2);
         }
-      } catch (error) {}
+        const p1TokenData = await contract.getPlayerToken(player01Address);
+        const p2TokenData = await contract.getPlayerToken(player02Address);
+        const player01 = await contract.getPlayer(player01Address);
+        const player02 = await contract.getPlayer(player02Address);
+        //player information
+        const p1Att = p1TokenData.attackStrength.toNumber();
+        const p1Def = p1TokenData.defenseStrength.toNumber();
+        const p1H = player01.playerHealth.toNumber();
+        const p1M = player01.playerMana.toNumber();
+
+        /* const p2Att = p2TokenData.attackStrength.toNumber();
+        const p2Def = p2TokenData.defenseStrength.toNumber(); */
+        const p2H = player02.playerHealth.toNumber();
+        const p2M = player02.playerMana.toNumber();
+
+        setPlayer1({
+          ...player01,
+          att: p1Att,
+          def: p1Def,
+          health: p1H,
+          mana: p1M,
+        });
+        setPlayer2({
+          ...player02,
+          att: X,
+          def: X,
+          health: p2H,
+          mana: p2M,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     };
     if (contract && gameData.activeBattle) getPlayerInfo();
   }, [contract, gameData, battleName]);
@@ -68,17 +108,45 @@ const Battle = () => {
     <div
       className={`${styles.flexBetween} ${styles.gameContainer} ${battleGround}`}
     >
-      <h1 className={styles.gameInfoHeading}> {battleName}</h1>
+      {/*  <h1 className={styles.gameInfoHeading}> {battleName}</h1>
 
       <div className={`${styles.flexCenter} flex-col`}>
-        <img src={player01} className={styles.gameLoadPlayerImg}></img>
+        <img src={player01img} className={styles.gameLoadPlayerImg}></img>
         <p className={styles.battleGroundCardText}>{player1}</p>
       </div>
       <h2 className={styles.gameLoadVS}>Vs</h2>
       <div className={`${styles.flexCenter} flex-col`}>
-        <img src={player02} className={styles.gameLoadPlayerImg}></img>
+        <img src={player02img} className={styles.gameLoadPlayerImg}></img>
         <p className={styles.battleGroundCardText}>{player2}</p>
+      </div> */}
+      {showAlert?.status && (
+        <Alert type={showAlert.type} message={showAlert.message} />
+      )}
+      <PlayerInfo player={player2} playerIcon={player02Icon} mt />
+      <div className={`${styles.flexCenter} flex-col my-10`}>
+        <Card card={player2} title={player2?.playerName} cardRef="" playerTwo />
+
+        <div className="flex items-center flex-row">
+          <ActionButton
+            imgUrl={attack}
+            handeClick={() => {}}
+            restStyles="mr-2 hover:border-yellow-400"
+          />
+          <Card
+            card={player1}
+            title={player1?.playerName}
+            cardRef=""
+            restStyles="mt-3"
+          />
+          <ActionButton
+            imgUrl={defense}
+            handeClick={() => {}}
+            restStyles="ml-6 hover:border-red-600"
+          />
+        </div>
       </div>
+      <PlayerInfo player={player1} playerIcon={player01Icon} mt />
+      <GameInfo />
     </div>
   );
 };
